b6b98fe9467849a50d27b63cb13484b5a6704607
Ahmad Heidari <Headria@gmail.com>
2021-12-10 10:13:01 +0330

    add loggerservice for catchs errors

--- src/controllers/tokensList.ts
+++ src/controllers/tokensList.ts
@@ -1,13 +1,24 @@
 import { TokenListServices } from "../services";
+import { LoggerService } from "../logger";
 
 export const TokensController = {
   getList: async (req: any, res: any) => {
-    const tokenList = await TokenListServices.getList({});
-    res.status(200).send({ code: 0, message: "", data: tokenList });
+    try {
+      const tokenList = await TokenListServices.getList({});
+      res.status(200).send({ code: 0, message: "", data: tokenList });
+    } catch (error) {
+      LoggerService.error(error);
+      res.status(500).send({});
+    }
   },
   searchToken: async (req: any, res: any) => {
-    const symbol = req.query?.symbol?.toLocaleLowerCase();
-    const tokenList = await TokenListServices.getList({ symbol });
-    res.status(200).send({ code: 0, message: "", data: tokenList });
+    try {
+      const symbol = req.query?.symbol?.toLocaleLowerCase();
+      const tokenList = await TokenListServices.getList({ symbol });
+      res.status(200).send({ code: 0, message: "", data: tokenList });
+    } catch (error) {
+      LoggerService.error(error);
+      res.status(500).send({});
+    }
   },
 };
