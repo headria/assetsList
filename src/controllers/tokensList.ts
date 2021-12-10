5731cfe162dc4509b7d0bc2219c87978ee507105
Ahmad Heidari <Headria@gmail.com>
2021-12-10 10:06:12 +0330

    change result format

--- src/controllers/tokensList.ts
+++ src/controllers/tokensList.ts
@@ -3,7 +3,7 @@ import { TokenListServices } from "../services";
 export const TokensController = {
   getList: async (req: any, res: any) => {
     const tokenList = await TokenListServices.getList({});
-    res.status(200).send({ tokenList });
+    res.status(200).send({ code: 0, data: tokenList });
   },
   searchToken: async (req: any, res: any) => {
     const symbol = req.query?.symbol?.toLocaleLowerCase();
