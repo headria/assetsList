38180b8f0ee16b59049467b87ca54647b268b351
Ahmad Heidari <Headria@gmail.com>
2021-12-12 18:11:00 +0330

    add format for price list

--- src/services/tokensList.ts
+++ src/services/tokensList.ts
@@ -29,17 +29,33 @@ export const TokenListServices = {
     symbols,
   }: {
     symbols?: string[];
-  }): Promise<IRawCurrencyTicker[]> => {
-    console.log({ symbols });
-    return nomics
-      .currenciesTicker({
-        ids: symbols,
-        interval: ["1h"],
+  }): Promise<any[]> => {
+    const priceList = await nomics.currenciesTicker({
+      ids: symbols,
+      interval: ["1h"],
+    });
+
+    let pList: any = [];
+    priceList.forEach((x: any) =>
+      pList.push({
+        symbol: x.symbol,
+        name: x.name,
+        price: x.price,
+        price_date: x.price_date,
+        price_timestamp: x.price_timestamp,
+        circulating_supply: x.circulating_supply,
+        max_supply: x.max_supply,
+        market_cap: x.market_cap,
+        volume: x["1h"]?.volume,
+        price_change: x["1h"]?.price_change,
+        price_change_pct: x["1h"]?.price_change_pct,
+        volume_change: x["1h"]?.volume_change,
+        volume_change_pct: x["1h"]?.volume_change_pct,
+        market_cap_change: x["1h"]?.market_cap_change,
+        market_cap_change_pct: x["1h"]?.market_cap_change_pct,
       })
-      .then((x) => x)
-      .catch((x) => {
-        console.log(x);
-        return x;
-      });
+    );
+
+    return pList;
   },
 };
