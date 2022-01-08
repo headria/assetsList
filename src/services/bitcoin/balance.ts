1a6f751452404f7507d60921f3ce37b427e7e91a
Ahmad Heidari <Headria@gmail.com>
2022-01-08 13:38:20 +0330

    [bitcoin] add confirmation for get balance

--- src/services/bitcoin/balance.ts
+++ src/services/bitcoin/balance.ts
@@ -1,6 +1,8 @@
 import getReuqest from "./callApi";
 
 export const getBalance = async (address: string): Promise<number> => {
-  const request: any = await getReuqest(`q/addressbalance/${address}`)();
+  const request: any = await getReuqest(`q/addressbalance/${address}`)({
+    confirmations: 6,
+  });
   return Number.parseFloat(request);
 };
