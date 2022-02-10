8eb950f2ca71e5266b6b9689acb3b09d3ea0f9ac
Ahmad Heidari <Headria@gmail.com>
2022-02-11 00:17:10 +0330

    add check for version

--- src/controllers/settings.ts
+++ src/controllers/settings.ts
@@ -1,9 +1,30 @@
 export const SettingsController = {
   checkForUpdate: async (req: any, res: any) => {
+    const version: number = req.qeury.version;
+
+    if (version === 2) {
+      return res.status(200).send({
+        code: 0,
+        message: "",
+        data: {
+          latestVersion: 0.1,
+          description: `
+          1. Add new coins
+          2. Bla bla bla
+          `,
+          forceUpdate: true,
+        },
+      });
+    }
     return res.status(200).send({
       code: 0,
       message: "",
-      data: false,
+      data: {
+        latestVersion: 0.1,
+        description: `
+        `,
+        forceUpdate: false,
+      },
     });
   },
 };
