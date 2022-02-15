209c2e3664686332894ebab6ac78c29de90857c1
Ahmad Heidari <Headria@gmail.com>
2022-02-16 00:01:25 +0330

    edit get balance of tokens erc20

--- src/services/ethereum/contracts.ts
+++ src/services/ethereum/contracts.ts
@@ -1,35 +1,27 @@
 import Web3 from "web3";
 import { AbiItem } from "web3-utils";
 import { LoggerService } from "../../logger";
+import { abiData } from "../../utils/abi";
 
 const provider =
-  "https://eth.getblock.io/mainnet/?api_key=2e89aac7-4985-4684-9547-fd4956bbd784";
+  "https://mainnet.infura.io/v3/05b9f03885ae4bc1b311e581352bcdbd";
 
 const Web3Client = new Web3(new Web3.providers.HttpProvider(provider));
-
-// The minimum ABI required to get the ERC20 Token balance
-const minABI: AbiItem[] = [
-  {
-    constant: true,
-    inputs: [{ name: "_owner", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ name: "balance", type: "uint256" }],
-    type: "function",
-  },
-];
 
 export const getBalanceERC20 = async (
   contractAddress: string,
   walletAddress: string
 ): Promise<string | undefined> => {
   try {
-    const contract = new Web3Client.eth.Contract(minABI, contractAddress);
-    const result = await contract.methods.balanceOf(walletAddress).call();
+    const contract = new Web3Client.eth.Contract(abiData, contractAddress);
+
+    const result = await contract.methods.balanceOf(walletAddress).call({});
 
     const format = Web3Client.utils.fromWei(result);
 
     return format;
-  } catch (e) {
-    LoggerService.error(e);
+  } catch (e: any) {
+    LoggerService.error(`[getBalanceERC20] -- err: ${e.toString()}`);
+    return "0";
   }
 };
