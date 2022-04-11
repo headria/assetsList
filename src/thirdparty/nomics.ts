3eb3ef05f7907514ed1177c4b7aec1daa73cb25a
Ahmad Heidari <Headria@gmail.com>
2022-04-11 21:41:28 +0300

    change nomics api

--- src/thirdparty/nomics.ts
+++ src/thirdparty/nomics.ts
@@ -2,7 +2,8 @@ import Nomics from "nomics";
 import querystring from "querystring";
 import axios from "axios";
 
-export const nomicsApiKey = "m_0dd1c06d4ad75bf3ab84e9e66e81243bd8d43b2b";
+// export const nomicsApiKey = "m_0dd1c06d4ad75bf3ab84e9e66e81243bd8d43b2b";
+export const nomicsApiKey = "4efb63666fce7fd21c13b973192afbff4f8348dd";
 
 const nomics = new Nomics({
   apiKey: nomicsApiKey,
