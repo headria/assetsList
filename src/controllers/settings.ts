9a851a66f0f641277b463a4d7fc37da5468feb59
headria <headria@gmail.com>
2022-09-27 18:31:18 +0300

    remove force update version

--- src/controllers/settings.ts
+++ src/controllers/settings.ts
@@ -2,20 +2,20 @@ export const SettingsController = {
   checkForUpdate: async (req: any, res: any) => {
     const version: number = req.query.version;
 
-    if (version <= 2) {
-      return res.status(200).send({
-        code: 0,
-        message: "",
-        data: {
-          latestVersion: 0.1,
-          description: `
-          1. Add new coins
-          2. Bla bla bla
-          `,
-          forceUpdate: true,
-        },
-      });
-    }
+    // if (version <= 2) {
+    //   return res.status(200).send({
+    //     code: 0,
+    //     message: "",
+    //     data: {
+    //       latestVersion: 0.1,
+    //       description: `
+    //       1. Add new coins
+    //       2. Bla bla bla
+    //       `,
+    //       forceUpdate: true,
+    //     },
+    //   });
+    // }
     return res.status(200).send({
       code: 0,
       message: "",
