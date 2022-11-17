77fe01272fdeeb12059b36af9c090938abd18f25
headria <headria@gmail.com>
2022-11-17 11:59:48 +0300

    remove lowercase for id

--- src/services/assets.ts
+++ src/services/assets.ts
@@ -4,11 +4,6 @@ import AssetsModel, { AssetsDTO } from "../models/assets";
 export const AssetsService = {
   getByContractAdderss: async (contractAddress: string) => {
     try {
-      (await AssetsModel.find({})).map(async (x, i) => {
-        x.id = x.id.toLowerCase();
-        console.log({ id: x.id, r: i });
-        await x.save();
-      });
       const token = await AssetsModel.findOne({
         id: contractAddress.toLowerCase(),
       });
